0e171938b35dc45bb027cd6989ee7b59e5a26b1a
natanael vinicius <162758578+natanaelvinicius@users.noreply.github.com>
2026-05-24 15:28:29 +0000

    adciona um video ao projeto

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-function home() {
-    return <h1>voce e carregado no valorant!</h1>
-}
+export default function Home() {
+  return (
+    <>
+      <h1>voce e carregado no valorant!</h1>
 
-export default home;
-
-function home () {
-    return (
-        <video src="/.video.mp4" controls/>
-
-    );
+      <video
+        src="/video.mp4"
+        controls
+        width="600"
+      />
+    </>
+  );
 }
