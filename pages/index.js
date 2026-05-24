2b7413f3543e604e09bddcc92631b421e30c7b92
natanael vinicius <162758578+natanaelvinicius@users.noreply.github.com>
2026-05-24 15:32:12 +0000

    adiciona um video ao projeto

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1>voce e carregado no valorant!</h1>
+      <h1>você é carregado no valorant!</h1>
 
       <video
         src="/video.mp4"
